7f98589cf6cf4f599206b77cda90fdcde353395d
avcopan <avcopan@gmail.com>
2026-04-29 22:04:25 -0400

    Add initial template

---
+++ tests/test_automol.py
@@ -0,0 +1,18 @@
+"""automol tests."""
+
+import automol
+
+
+def test_stub() -> None:
+    """Stub test to ensure the test suite runs."""
+    print(automol.__version__)  # noqa: T201
+
+
+def test__greet() -> None:
+    """Test the greet function."""
+    assert automol.greet("World") == "Hello, World!"
+
+
+def test__greet_jim() -> None:
+    """Test the greet_jim function."""
+    assert automol.greet_jim() == "Hello, Jim!"
